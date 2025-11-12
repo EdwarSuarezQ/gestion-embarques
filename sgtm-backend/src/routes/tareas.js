e4eae5d8d6f8258d4e74cb2fec0b28d8007a753f
edwarSuarezQ <145814931+edwarSuarezQ@users.noreply.github.com>
2025-11-12 01:26:14 -0500

    correciones

--- sgtm-backend/src/routes/tareas.js
+++ sgtm-backend/src/routes/tareas.js
@@ -1,38 +1,38 @@
-const express = require('express');
-const { body } = require('express-validator');
+const express = require("express");
+const { body } = require("express-validator");
 const router = express.Router();
-const tareasController = require('../controllers/tareasController');
-const auth = require('../middleware/auth');
+const tareasController = require("../controllers/tareasController");
+const auth = require("../middleware/auth");
 
 // Create
 router.post(
-  '/',
+  "/",
   [
     auth,
-    body('titulo').notEmpty().withMessage('titulo requerido'),
-    body('fecha')
+    body("titulo").notEmpty().withMessage("titulo requerido"),
+    body("fecha")
       .matches(/^\d{2}\/\d{2}\/\d{4}$/)
-      .withMessage('fecha inválida'),
+      .withMessage("fecha inválida"),
   ],
-  tareasController.create,
+  tareasController.create
 );
 
 // List
-router.get('/', auth, tareasController.list);
+router.get("/", auth, tareasController.list);
 
 // Get
-router.get('/:id', auth, tareasController.get);
+router.get("/:id", auth, tareasController.get);
 
 // Update (put)
-router.put('/:id', auth, tareasController.update);
+router.put("/:id", auth, tareasController.update);
 
 // Patch
-router.patch('/:id', auth, tareasController.patch);
+router.patch("/:id", auth, tareasController.patch);
 
 // Delete
-router.delete('/:id', auth, tareasController.remove);
+router.delete("/:id", auth, tareasController.remove);
 
 // Estadísticas
-router.get('/estadisticas', auth, tareasController.stats);
+router.get("/estadisticas", auth, tareasController.stats);
 
 module.exports = router;
